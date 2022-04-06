7be01e7b002708e68e5bace2790876ffb47c71d4
laven <ldavis@pymblelc.nsw.edu.au>
2022-04-06 20:48:17 +1000

    set locale is a bitch

--- js/major.js
+++ js/major.js
@@ -207,7 +207,7 @@ $(function () {
 
     var successMessage = "Thank you!";
 
-    $.datetimepicker.setLocale('en');
+    $.datetimepicker.setLocale();
 
     if (!Modernizr.inputtypes.datetime) {
         $("input[data-type=date]").datetimepicker({ timepicker: false, format: "Y/m/d" }).attr("type", "text");
