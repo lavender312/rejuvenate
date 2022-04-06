7edf009a9f6dae5692330646917e97fdf68b9c60
laven <ldavis@pymblelc.nsw.edu.au>
2022-04-06 20:55:48 +1000

    back

--- js/major.js
+++ js/major.js
@@ -207,7 +207,7 @@ $(function () {
 
     var successMessage = "Thank you!";
 
-   // $.datetimepicker.setLocale('en');
+   $.datetimepicker.setLocale('en');
 
     if (!Modernizr.inputtypes.datetime) {
         $("input[data-type=date]").datetimepicker({ timepicker: false, format: "Y/m/d" }).attr("type", "text");
