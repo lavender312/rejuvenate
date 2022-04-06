ee1f8958bc5f1ac6241e95c81a8767387b05e43c
laven <ldavis@pymblelc.nsw.edu.au>
2022-04-06 15:22:03 +1000

    auth0 javascript

--- js/major.js
+++ js/major.js
@@ -124,57 +124,107 @@ var found = false;
 });*/
 
 //login through authfication
-/*
-var lock = new Auth0Lock(AUTH0_CLIENT_ID, AUTH0_DOMAIN, {
-    auth: {
-        params: { scope: 'openid email profile' },
-        configurationBaseUrl: 'https://cdn.auth0.com',
-        esponseType: 'token id_token'
-    }
-});
+// Google and standalone authentication using Auth0 integration with restdb.io
 
-$("#btnLogIn").click(
-    function (e) {
+$(function () {
+    const AUTH0_CLIENT_ID = "XXK1MFFk3DwMidOeqZuKJvYxuXXEnDR0";
+    const AUTH0_DOMAIN = "lavenmajor.au.auth0.com";
+
+    var lock = new Auth0Lock(AUTH0_CLIENT_ID, AUTH0_DOMAIN, {
+        auth: {
+            params: { scope: 'openid email profile' },
+            configurationBaseUrl: 'https://cdn.auth0.com',
+            responseType: 'token id_token'
+        }
+    });
+    // auto login
+    if (localStorage.getItem('id_token')) {
+        var token = localStorage.getItem('id_token');
+        lock.getProfile(token, function (error, profile) {
+            if (error) {
+                // Handle error
+                return;
+            }
+            // Display user information
+            show_profile_info(profile);
+
+            // global ajax Authorization setup
+            $.ajaxPrefilter(function (options) {
+                if (!options.beforeSend) {
+                    options.beforeSend = function (xhr) {
+                        xhr.setRequestHeader('Authorization', 'Bearer ' + localStorage.getItem('id_token'));
+                    }
+                }
+            });
+
+            // get task items from database
+            getItems();
+        });
+    }
+    var show_profile_info = function (profile) {
+        console.log(profile.picture);
+        $('.nickname').text(profile.nickname);
+        $('.btn-login').hide();
+        $('.avatar').attr('src', profile.picture).show();
+        $('.btn-logout').show();
+    };
+
+    var retrieve_profile = function () {
+        var id_token = localStorage.getItem('id_token');
+        if (id_token) {
+            lock.getProfile(id_token, function (err, profile) {
+                if (err) {
+                    return alert('There was an error getting the profile: ' + err.message);
+                }
+                // Display user information
+                show_profile_info(profile);
+                // enable api button
+                $('.btn-api').removeAttr("disabled");
+            });
+        }
+    };
+
+    $('.btn-login').click(function (e) {
         e.preventDefault();
         lock.show();
         return false;
-    }
-);
+    });
 
-lock.on("authenticated", function(authResult) {
-    lock.getProfile(authResult.accessToken, function(error, profile) {
-      if (error) {
-          // TODO: Handle error
-          console.log("Auth0 getProfile failed", error);
-          return;
-      }
-                  
-      localStorage.setItem('id_token', authResult.idToken);
-  
-      // Display user information
-      show_profile_info(profile);
-  
-      // global ajax Authorization setup
-      $.ajaxPrefilter(function( options ) {
-          if ( !options.beforeSend) {
-              options.beforeSend = function (xhr) { 
-                  xhr.setRequestHeader('Authorization', 'Bearer ' + localStorage.getItem('id_token'));
-              }
-          }
-      });
-  
-      // get task items from database
-      getIrritants();
+    $('.btn-logout').click(function (e) {
+        localStorage.removeItem('id_token');
+        $('.btn-api').attr("disabled", "true");
+        window.location.href = "/";
+        e.preventDefault();
+        return false;
+    });
+
+    lock.on("authenticated", function (authResult) {
+        lock.getProfile(authResult.accessToken, function (error, profile) {
+            if (error) {
+                // Handle error
+                console.log("Auth0 getProfile failed", error);
+                return;
+            }
+
+            localStorage.setItem('id_token', authResult.idToken);
+
+            // Display user information
+            show_profile_info(profile);
+
+            // global ajax Authorization setup
+            $.ajaxPrefilter(function (options) {
+                if (!options.beforeSend) {
+                    options.beforeSend = function (xhr) {
+                        xhr.setRequestHeader('Authorization', 'Bearer ' + localStorage.getItem('id_token'));
+                    }
+                }
+            });
+
+            // get task items from database
+            getItems();
+        });
     });
 });
-
-/* change item stuff to irritant stuff
-// get tasks from database
-function getIrritants(){
-  $.getJSON('https://myapp-7175.restdb.io/rest/items', function(data){...});}
-/*
-
-  /*
 
 //irritant log
 
